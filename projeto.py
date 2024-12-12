e754132f46c50b32df601b54ac1110bc99ffb060
Paula Tamie Entringer <paulaetgr92@gmail.com>
2024-12-12 14:37:26 -0300

    Update projeto.py

--- projeto.py
+++ projeto.py
@@ -6,30 +6,29 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 import time
 
-# Configuração do driver
+
 service = Service('C:/Users/paula/OneDrive/Área de Trabalho/chromedriver.exe')
 driver = webdriver.Chrome(service=service)
 
 try:
-    # Abrindo a URL
+    
     driver.get('https://account.impacta.edu.br/')
 
-    # Inserindo login
+
     login = WebDriverWait(driver, 10).until(
         EC.visibility_of_element_located((By.XPATH, '/html/body/div[1]/div/div[2]/form/div[1]/div/input'))
     )
-    login.send_keys("2401147")
+    login.send_keys("****")
 
-    # Inserindo senha
+    
     senha = WebDriverWait(driver, 10).until(
         EC.visibility_of_element_located((By.XPATH, '/html/body/div[1]/div/div[2]/form/div[2]/div/input'))
     )
-    senha.send_keys("Segredinho3297")
+    senha.send_keys("*****")
 
     print("Por favor, resolva o reCAPTCHA manualmente e pressione Enter para continuar...")
-    input()  # Pausa até que o usuário pressione Enter
-
-    # Fechar modal se existir
+    input()  
+    
     try:
         close_button = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.CSS_SELECTOR, '.swal2-close'))
@@ -39,17 +38,16 @@ try:
     except TimeoutException:
         print("Nenhum modal foi encontrado.")
 
-    # Confirmar o login
+
     confirm_button = WebDriverWait(driver, 10).until(
         EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div/div[2]/form/div[4]/button'))
     )
     
-    # Rolando para o botão e clicando
+  
     driver.execute_script("arguments[0].scrollIntoView(true);", confirm_button)
-    time.sleep(1)  # Aguardar o botão ficar visível
+    time.sleep(1)
     confirm_button.click()
 
-    # Verificar redirecionamento ou mudanças após o login
     print("Aguardando redirecionamento ou mudanças na página...")
     try:
         elemento_pos_login = WebDriverWait(driver, 20).until(
@@ -58,8 +56,7 @@ try:
         print("Login bem-sucedido! Página carregada.")
     except TimeoutException:
         print("Nenhuma mudança detectada dentro do tempo especificado. Verifique o login ou o redirecionamento.")
-    
-    # Fechar pop-up de outro alerta após o login
+
     try:
         feche_button = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, '/html/body/div[4]/div[3]/button'))  # Ajuste o caminho do botão de fechar se necessário
@@ -69,7 +66,6 @@ try:
     except TimeoutException:
         print("Nenhum pop-up foi encontrado após o login.")
     
-    # Tentar acessar o botão "Financeiro Online"
     try:
         financeiro_button = WebDriverWait(driver, 10).until(
             EC.element_to_be_clickable((By.XPATH, '/html/body/div[2]/div/div[1]/ul[1]/li[5]/a'))
@@ -87,7 +83,7 @@ try:
         financeiro2_button.click()
     except TimeoutException:
         print("Não foi possível encontrar o botão 'Financeiro Online'.")
-    # Tentar acessar o botão de "print_recibo"
+
 
     try:
         popup_button = WebDriverWait(driver, 10).until(
@@ -97,7 +93,7 @@ try:
         popup_button.click()
     except TimeoutException:
         print("Não foi possível encontrar o botão do pop-up.")
-    # Tentar acessar o botão de "print_recibo"
+
 
     try:
         print_button = WebDriverWait(driver, 10).until(
@@ -109,6 +105,5 @@ try:
         print("Não foi possível encontrar o botão 'Print Recibo'.")
 
 finally:
-    # Esperar alguns segundos antes de encerrar
     time.sleep(5)
     driver.quit()
